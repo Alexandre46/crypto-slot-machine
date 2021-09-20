438bc0f9cbf370345bc80085024fe9b51d75f4fb
Alexandre Abreu <alexandre.abreu@nearsoftsolutions.com>
2021-09-20 22:48:07 +0100

    Fix server

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,5 +1,10 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
-    app.use(createProxyMiddleware("/api", { target: "http://localhost:3000" }));
+    app.use(createProxyMiddleware("/api",{ 
+      target: "http://[::1]:3000",
+      headers: {
+        "Connection": "keep-alive"
+      }
+     }));
 };
