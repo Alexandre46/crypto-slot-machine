34bdc46074e070e866730071fc35539424a0b923
Alexandre Abreu <alexandre.abreu@nearsoftsolutions.com>
2021-09-20 23:00:18 +0100

    Fix server

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,9 +2,11 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
     app.use(createProxyMiddleware("/api",{ 
-      target: "https://[::1]:3000",
+      target: "http://[::1]:3000",
       headers: {
         "Connection": "keep-alive"
-      }
+      },
+      changeOrigin: true,
+      logLevel: 'debug',
      }));
 };
