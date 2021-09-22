bddf5ca444656735f9db2bae1638bee3d03ffb1a
Alexandre Abreu <alexandre.abreu@nearsoftsolutions.com>
2021-09-22 08:25:35 +0100

    Fix proxy

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,5 +1,12 @@
-const { proxy } = require("http-proxy-middleware");
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
-module.exports = app => {
-  app.use(proxy("/api/*", { target: "http://localhost:3000/" }));
+module.exports = function(app) {
+    app.use(
+      '/api',
+      createProxyMiddleware({
+        target: "http://[::1]:3000",
+        changeOrigin: true,
+        secure: false
+     })
+    );
 };
